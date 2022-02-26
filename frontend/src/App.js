49f299043a9cc39c18d42281b88e20701fcd5e32
JaberHPranto <jhpranto53@gmail.com>
2022-02-26 20:46:05 +0600

    🔨 done with file and folder structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,17 @@
+import "./App.scss";
+import { Navbar } from "./components";
+import { About, Footer, Header, Skills, Work } from "./container";
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;

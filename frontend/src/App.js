c70e5d8f1394b06237ba79a4e5e1e93527722934
JaberHPranto <jhpranto53@gmail.com>
2022-03-02 21:18:13 +0600

    🔨 update header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import "./App.scss";
 import { Navbar } from "./components";
-import { About, Footer, Header, Skills, Work } from "./container";
+import { About, Footer, Skills, UpdatedHeader, Work } from "./container";
 function App() {
   return (
     <div className="app">
       <Navbar />
-      <Header />
+      <UpdatedHeader />
       <About />
       <Work />
       <Skills />
---
+++ frontend/src/container/Header/UpdatedHeader.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { images } from "../../constants";
+import "./UpdatedHeader.scss";
+
+function UpdatedHeader() {
+  return (
+    <>
+      <div className="header__container">
+        <div className="header__col-1">
+          <div className="header__name">
+            <h2>👋 Hello, I'm Pranto</h2>
+          </div>
+          <div className="header__desc">
+            I love to build <span>fun</span> and <span>interactive</span> things
+            for the web
+          </div>
+        </div>
+        <div className="header__col-3"></div>
+        <div className="header__col-2">
+          <div className="header__hero-img">
+            <img src={images.hero} alt="hero" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default UpdatedHeader;
